9f6e19f7949c8156eb6a8f386dc63b6658b53bb7
Elida Marukyan <elida.marukyan@digitain.com>
2023-05-16 14:31:52 +0400

    initial commit

---
+++ cashierScript.js
@@ -0,0 +1,35 @@
+(function() {
+  let iframe = null;
+  let wrapper = null;
+
+  function sendData(data) {
+    function onLoad() {
+      iframe.contentWindow.postMessage(data, '*');
+    }
+    iframe.addEventListener('load', onLoad);
+    iframe.contentWindow.postMessage(data, '*');
+  }
+
+  window.CashierScripts = {
+    init(wrapperId) {
+      wrapper = document.getElementById(wrapperId)
+          || document.body.appendChild(document.createElement('div'));
+      const height = '600';
+      iframe = document.createElement('iframe');
+      iframe.setAttribute('frameborder', '0');
+      iframe.height = height;
+      iframe.style.minWidth = '100%';
+      iframe.style.display = 'block';
+      iframe.src = 'http://pycashier.dev.digi.loc/paymentsAccount';
+      wrapper.appendChild(iframe);
+    },
+
+    authorize(authData) {
+      sendData(authData);
+    },
+
+    setStyles(theme) {
+      sendData(theme);
+    }
+  };
+})();
